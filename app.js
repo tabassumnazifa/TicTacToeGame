f247a1055e9615d4b5e045523d65cfceacc41908
Nazifa Tabassum <tnazifa003@gmail.com>
2025-08-25 22:10:08 +0600

    set game

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
-let boxes= document.querySelectorAll(".box");
-let reset= document.querySelector("#reset btn");
-let turnO=true;
-const winpart=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
+let boxes = document.querySelectorAll(".box");
+let turnO = true;
+let winner = false;
+let boxcnt=0;
+let popup = document.getElementById("popup");
+let popupMessage = document.getElementById("popup-message");
+let closePopup = document.getElementById("close-popup");
+let turn=document.getElementById("turn");
+const winpart = [
+    [0,1,2],[0,3,6],[0,4,8],
+    [1,4,7],[2,5,8],[2,4,6],
+    [3,4,5],[6,7,8]
 ];
+
 boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        console.log("Box Was Clicked");
-        if(turnO==true)
-           {
-                box.innerText="0";
-                turnO=false;
-           } 
-        else
-           {
-                 box.innerText="X";
-                turnO=true;
-           } 
-        box.disabled=true;
+    box.addEventListener("click", () => {            
+        if(winner) return;
+
+        box.innerText = turnO ? "0" : "X";
+        turnO = !turnO;
+        box.disabled = true;
+        boxcnt++;
+
         checker();
     });
+    
 });
-const checker=() =>{
-for(let it of winpart){
-let pos1=boxes[it[0]].innerText;
-let pos2=boxes[it[1]].innerText;
-let pos3=boxes[it[2]].innerText;
+
+const checker = () => {  player();
+    for(let it of winpart){
+        let pos1 = boxes[it[0]].innerText;
+        let pos2 = boxes[it[1]].innerText;
+        let pos3 = boxes[it[2]].innerText;
+
+        if(pos1 && pos2 && pos3 && pos1 === pos2 && pos2 === pos3){
+            winner = true;
+            showpop("Player " + pos1 + " Wins!");
+            disableAllBoxes();
+            return;
+        }
+    }
+    if(boxcnt==9 && !winner){
+         showpop("It's A Tie!");
+    }
+    
 }
 
+const disableAllBoxes = () => {
+    boxes.forEach((dis) => dis.disabled = true);
 }
+
+const showpop = (message) => {
+    popupMessage.innerText = message;
+    popup.style.display = "flex";
+}
+const player=()=>{
+    turn.innerText = "Player " + (turnO ? "0" : "X") + "'s Turn";
+    const turnMessage=turn.innerText;
+}
+closePopup.addEventListener("click", () => {
+    popup.style.display = "none"; // hide popup
+});
